a768a7146a1d863b2dbf7e4e1ac7f1c8db86a4a2
jamesjn <james.jn.chiang@gmail.com>
2012-06-02 23:54:24 -0400

    Add retrieving of json from server

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    group_list_controller = GroupListController.alloc.init
+    @window.rootViewController = group_list_controller
+    @window.makeKeyAndVisible
     true
   end
 end
---
+++ app/group_list_controller.rb
@@ -0,0 +1,26 @@
+class GroupListController < UITableViewController
+  def viewDidLoad
+    load_groups_from_server
+  end  
+
+  def load_groups_from_server
+    @groups = [] 
+    BubbleWrap::HTTP.get("http://www.jamesjn.com/simpleapp/groups.json") do |response|
+      @groups = BubbleWrap::JSON.parse(response.body.to_str)
+      view.reloadData
+    end
+  end
+
+  def tableView(tableView, numberOfRowsInSection:section)
+    @groups.size
+  end
+  
+  CellID = 'CellIdentifier'
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = tableView.dequeueReusableCellWithIdentifier(CellID) || UITableViewCell.alloc.initWithStyle(UITableViewCellStyleSubtitle, reuseIdentifier:CellID)
+    group = @groups[indexPath.row]
+    cell.textLabel.text = group["name"] 
+    cell
+  end
+
+end

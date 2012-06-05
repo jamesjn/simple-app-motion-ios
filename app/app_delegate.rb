017095845cda9663adad786b0a73dc9040a654e9
jamesjn <james.jn.chiang@gmail.com>
2012-06-05 19:23:43 -0400

    add background for navigation bar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,6 +3,8 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     group_list_controller = GroupListController.alloc.init
     group_nav_controller = UINavigationController.alloc.initWithRootViewController(group_list_controller)
+    group_nav_controller.navigationBar.setBackgroundImage(UIImage.imageNamed("brushedmetal.png"), forBarMetrics:(UIBarMetricsDefault))
+    #group_nav_controller.navigationBar.setTitleTextAttributes({UITextAttributeTextColor: UIColor.blackColor, UITextAttributeFont: UIFont.fontWithName("AmericanTypewriter", size:18)})
     @window.rootViewController = group_nav_controller
     @window.makeKeyAndVisible
     true

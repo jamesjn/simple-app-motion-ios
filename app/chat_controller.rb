b398380483fede566a2a74f7a2e9a84d537eb3a8
jamesjn <james.jn.chiang@gmail.com>
2012-07-15 09:21:11 -0400

    add textfield and sending of events

--- app/chat_controller.rb
+++ app/chat_controller.rb
@@ -3,12 +3,36 @@ class ChatController< UIViewController
   def init
     start_socketio
     chat_textview = create_chat_textview
+    chat_textfield = create_chat_textfield
     view.addSubview(chat_textview)
+    view.addSubview(chat_textfield)
     self.tabBarItem = UITabBarItem.alloc.initWithTitle('Chat', image:UIImage.imageNamed('star.png'), tag:1)
     super
   end
 
   def viewDidLoad
+  end
+
+  def create_chat_textfield
+    chat_textfield = UITextField.new
+    chat_textfield.textColor = UIColor.blackColor
+    chat_textfield.text = "please enter text"
+    chat_textfield.backgroundColor = UIColor.whiteColor
+    chat_textfield.frame = [[0,0], [320,25]]
+    chat_textfield.delegate = self
+    @chat_textfield = chat_textfield
+    chat_textfield
+  end
+
+  def textFieldShouldReturn(theTextField)
+    if theTextField == @chat_textfield
+      @chat_textfield.resignFirstResponder
+      msg = @chat_textfield.text
+      @chat_textview.text =  msg + "\n" + @chat_textview.text 
+      @socketIO.sendEvent("message", withData: {message: msg})
+      @chat_textfield.text = ''
+    end
+    true
   end
 
   def create_chat_textview
@@ -18,15 +42,21 @@ class ChatController< UIViewController
     chat_textview.editable = false
     chat_textview.text = "chat goes here"
     chat_textview.backgroundColor = UIColor.clearColor
-    chat_textview.frame = [[0,0], [400, 400]]
+    chat_textview.frame = [[0,25], [320, 330]]
+    @chat_textview = chat_textview
     chat_textview
+  end
+
+  def socketIO(socket, didReceiveEvent:packet)
+    if packet.name == 'message'
+      @chat_textview.text =  packet.args.first + "\n" + @chat_textview.text 
+    end
   end
 
   def start_socketio
     @socketIO = SocketIO.alloc.initWithDelegate(self)
     @socketIO.connectToHost("jamesjn.com", onPort:8080);
     @socketIO.sendEvent("join", withData:{name: "iphone"})
-    @socketIO.sendEvent("message", withData: {message: "hello world"})
   end
 
   def socketIO(socket, didReceiveMessage:packet)
